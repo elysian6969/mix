8545d148ed5f5e1b1b383c0cef0ac68ea2ec463f
tsuji <tsujianii@tuta.io>
2021-09-25 17:10:30 +0800

    allow hyphens in ids

--- crates/id/src/util.rs
+++ crates/id/src/util.rs
@@ -1,9 +1,13 @@
 use crate::{Error, ErrorKind};
 
 pub fn validate(id: &str, kind: ErrorKind) -> Result<(), Error> {
-    if let Some(index) = id.chars().position(|c| !c.is_ascii_alphanumeric()) {
+    if let Some(index) = id.chars().position(|c| !is_valid(c)) {
         Err(Error::UnexpectedCharacter { index, kind })
     } else {
         Ok(())
     }
+}
+
+fn is_valid(c: char) -> bool {
+    c.is_ascii_alphanumeric() || c == '-'
 }
